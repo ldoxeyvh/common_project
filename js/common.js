eba96bdd4d5fa5f1040d1576299e64034a84d9b2
Leif Doxey-Van Heest <ldoxeyvh@umich.edu>
2023-11-20 17:05:34 -0500

    reduce motion test

---
+++ js/common.js
@@ -0,0 +1,4 @@
+const font = document.createElement("link");
+font.rel = "stylesheet"
+font.href = "https://fonts.googleapis.com/css?family=Nunito"
+document.querySelector("head").appendChild(font);
